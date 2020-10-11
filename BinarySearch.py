6f46f21f46b4fac71862983ac8f6055336fc9723
Navneet Kaur <67039595+Navneet123-del@users.noreply.github.com>
2020-10-11 22:08:41 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -4,15 +4,15 @@ import pymysql
 connection = pymysql.connect(host="localhost", user="***", passwd="******", db="cse")
 #use your mysql username and password in place of ****
 mycursor = connection.cursor()
-anurag=mycursor.execute("select Roll_No from electives")
+anurag=mycursor.execute("Select Roll_No from electives")
 anurag = mycursor.fetchall()
 anurag = [int(item[0]) for item in anurag]
 anurag.sort()
 print('Unsorted list is :',anurag)
 x=int(len(anurag))
 print('Sorted list is:',anurag)
-print('Length of list is:',len(anurag))
-a=int(input("Enter Roll_No to search: "))
+print('Length of list is :',len(anurag))
+a=int(input("Enter Roll_No to search:"))
 
 def binary_search(anurag, x, a):
     start = 0
